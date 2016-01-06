771a474446cf3aad5d04cfac90cc04e3d15a535d
Abhinav Mishra <abhinav@Abhinavs-MacBook-Pro.local>
2016-01-07 02:07:30 +0530

    site controller
    chap 7

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -75,4 +75,13 @@ class LoginForm extends Model
 
         return $this->_user;
     }
+
+    public function loginAdmin() {
+        if (($this->validate()) && $this->getUser()->role_id >= ValueHelpers::getRoleValue('Admin')
+        && $this->getUser()->status_id == ValueHelpers::getStatusValue('Active')) {
+        return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+        } else {
+        throw new NotFoundHttpException('You Shall Not Pass.');
+        } 
+    }
 }
